ef92c660a9c3a052390b0162849280be58068086
m7mdragab <mr341349@gmail.com>
2024-11-26 08:58:54 +0200

    start create splash view body

---
+++ lib/core/utils/assets_data.dart
@@ -0,0 +1,5 @@
+class AssetsData {
+  static const logo = 'assets/images/logo.svg';
+  static const vector = 'assets/images/Vector.svg';
+  static const vector1 = 'assets/images/Vector1.svg';
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_translate_clone/core/utils/assets_data.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Stack(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SvgPicture.asset(
+              AssetsData.logo,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
