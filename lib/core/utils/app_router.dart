c25ec1fa6d1ce087fe9b4ec50c899a9e52db8e94
m7mdragab <mr341349@gmail.com>
2024-12-01 22:55:40 +0200

    done splash view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_translate_clone/features/home/presentation/views/home_view.dart';
 import 'package:google_translate_clone/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
+  static const homeView = '/homeView';
+
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return const SplashView();
+        },
+      ),
+      GoRoute(
+        path: homeView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeView();
         },
       ),
     ],
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_translate_clone/core/utils/app_router.dart';
 import 'package:google_translate_clone/core/utils/assets_data.dart';
+import 'package:google_translate_clone/features/splash/presentation/views/widgets/sliding_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    initSlideAnimation();
+    navigateToHome();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +41,28 @@ class SplashViewBody extends StatelessWidget {
             SvgPicture.asset(
               AssetsData.logo,
             ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
+            ),
+            SlidingText(slidingAnimation: slidingAnimation)
           ],
         ),
       ],
     );
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).push(AppRouter.homeView);
+    });
+  }
+
+  void initSlideAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
   }
 }
