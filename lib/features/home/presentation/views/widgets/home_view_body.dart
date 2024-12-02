4061f85b65cc7d4c536de24689df59d907ef2845
m7mdragab <mr341349@gmail.com>
2024-12-02 22:54:32 +0200

    create language button

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_translate_clone/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Enter text to translate:",
+                  style: Styles.textStyle20.copyWith(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
