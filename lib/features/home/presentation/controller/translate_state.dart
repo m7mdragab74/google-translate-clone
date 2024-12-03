a5877131f088b24b7b8d6f5e3db8ad884405b833
m7mdragab <mr341349@gmail.com>
2024-12-03 12:44:30 +0200

    create translate state

--- lib/features/home/presentation/controller/translate_state.dart
+++ lib/features/home/presentation/controller/translate_state.dart
@@ -1,1 +1,21 @@
+import 'package:google_translate_clone/features/home/presentation/view_model/translate_model.dart';
+
 class TranslateState {}
+
+final class TranslateInitialState extends TranslateState {}
+
+final class TranslateLoadingState extends TranslateState {}
+
+final class TranslateSuccessState extends TranslateState {
+  final TranslationModel translationModel;
+
+  TranslateSuccessState(this.translationModel);
+}
+
+final class TranslateFailureState extends TranslateState {
+  final String message;
+
+  TranslateFailureState(this.message);
+}
+
+class TranslateLanguageUpdatedState extends TranslateState {}
